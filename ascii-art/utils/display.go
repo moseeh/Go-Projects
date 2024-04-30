782eb6599fed063770f9dd454e8f7173f62eafe8
moonyango <mosesadrian825@gmail.com>
2024-04-30 13:55:31 +0300

    more changes

--- ascii-art/utils/display.go
+++ ascii-art/utils/display.go
@@ -6,6 +6,14 @@ import (
 )
 
 func DisplayText(input string, contentLines []string) {
+	if input == "" {
+		return
+	}
+	if input == "\\n" || input == "\n" {
+		fmt.Println()
+		return
+	}
+
 	input = strings.ReplaceAll(input, "\n", "\\n")
 	input = strings.ReplaceAll(input, "\\t", "    ")
 	wordslice := strings.Split(input, "\\n")
