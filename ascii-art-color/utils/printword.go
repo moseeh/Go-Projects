4f81486e3e1a8900e6534f6d157753079d3abe35
moonyango <mosesadrian825@gmail.com>
2024-06-02 12:49:34 +0300

    changed printword function to work for substrings to be colored

--- ascii-art-color/utils/printword.go
+++ ascii-art-color/utils/printword.go
@@ -8,18 +8,25 @@ import (
 // printWord prints a word using predefined lines from the content file
 func PrintWord(color string, word string, tobecolored string, contentLines []string) {
 	linesOfSlice := make([]string, 9)
-	for _, v := range word {
-		isColored := strings.ContainsRune(tobecolored, v)
-		if isColored{
-			for i := 1; i <= 9; i++ {
-				linesOfSlice[i-1] += Color(contentLines[int(v-32)*9+i], color)
-			}
-		} else {
-			for i := 1; i <= 9; i++ {
-				linesOfSlice[i-1] += contentLines[int(v-32)*9+i]
-			}
-		}
+	start := 0
 
+	for start < len(word) {
+		if strings.HasPrefix(word[start:], tobecolored) {
+			for i := 0; i < 9; i++ {
+				for _, v := range tobecolored {
+					linesOfSlice[i] += Color(contentLines[int(v-32)*9+i], color)
+				}
+			}
+			start += len(tobecolored)
+		} else {
+			v := rune(word[start])
+			for i := 0; i < 9; i++ {
+				linesOfSlice[i] += contentLines[int(v-32)*9+i]
+			}
+			start++
+		}
 	}
+
 	fmt.Println(strings.Join(linesOfSlice, "\n"))
 }
+
