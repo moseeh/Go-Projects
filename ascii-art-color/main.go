1bb9e3cac345497fa7af27cb2fb8da78083e7419
moonyango <mosesadrian825@gmail.com>
2024-06-05 17:21:09 +0300

    minor changes

--- ascii-art-color/main.go
+++ ascii-art-color/main.go
@@ -1,16 +1,17 @@
 package main
 
 import (
-	"color/utils"
 	"fmt"
 	"os"
 	"strings"
+
+	"color/utils"
 )
 
 func main() {
-	//check enough command line arguments
+	// check enough command line arguments
 	if len(os.Args[1:]) < 1 || len(os.Args[1:]) > 3 || (!strings.HasPrefix(os.Args[1], "--color=") && len(os.Args[1:]) != 1) || (strings.HasPrefix(os.Args[1], "--color=") && len(os.Args[1:]) == 1) {
-		fmt.Print("Usage: go run . [OPTION] [STRING]\n\n")
+		fmt.Printf("Usage: go run . [OPTION] [STRING]\n\n")
 		fmt.Println("EX: go run . --color=<color> <letters to be colored> \"something\"")
 		return
 	}
@@ -26,6 +27,11 @@ func main() {
 	// checks if the file provided contains 856 lines
 	if len(contentLines) != 856 {
 		fmt.Println("invalid text file")
+		return
+	}
+	_, errcolor := utils.FindColor(color)
+	if errcolor != nil {
+		fmt.Println(errcolor , color)
 		return
 	}
 	utils.DisplayText(color, inputslice, contentLines)
--- ascii-art-color/utils/printword.go
+++ ascii-art-color/utils/printword.go
@@ -7,7 +7,6 @@ import (
 
 // printWord prints a word using predefined lines from the content file
 func PrintWord(color string, word string, tobecolored string, contentLines []string) {
-
 	if tobecolored == "" {
 		// If the substring to be colored is empty, just print the word without coloring
 		linesOfSlice := make([]string, 8)
@@ -21,21 +20,35 @@ func PrintWord(color string, word string, tobecolored string, contentLines []str
 	}
 	linesOfSlice := make([]string, 8)
 	start := 0
-
-	for start < len(word) {
-		if strings.HasPrefix(word[start:], tobecolored) {
-			for i := 1; i < 9; i++ {
-				for _, v := range tobecolored {
+	if strings.Contains(word, tobecolored) && len(tobecolored) > 1 {
+		for start < len(word) {
+			if strings.HasPrefix(word[start:], tobecolored) {
+				for i := 1; i < 9; i++ {
+					for _, v := range tobecolored {
+						linesOfSlice[i-1] += Color(contentLines[int(v-32)*9+i], color)
+					}
+				}
+				start += len(tobecolored)
+			} else {
+				v := rune(word[start])
+				for i := 1; i < 9; i++ {
+					linesOfSlice[i-1] += contentLines[int(v-32)*9+i]
+				}
+				start++
+			}
+		}
+	} else {
+		for _, v := range word {
+			isColored := strings.ContainsRune(tobecolored, v)
+			if isColored {
+				for i := 1; i < 9; i++ {
 					linesOfSlice[i-1] += Color(contentLines[int(v-32)*9+i], color)
 				}
+			} else {
+				for i := 1; i < 9; i++ {
+					linesOfSlice[i-1] += contentLines[int(v-32)*9+i]
+				}
 			}
-			start += len(tobecolored)
-		} else {
-			v := rune(word[start])
-			for i := 1; i < 9; i++ {
-				linesOfSlice[i-1] += contentLines[int(v-32)*9+i]
-			}
-			start++
 		}
 	}
 
