8c4483ac7294d8e731d94c5439d118ff652794cc
moseeh <mosesadrian825@gmail.com>
2024-06-10 11:33:52 +0300

    Merge: copy files from ascii-art directory

---
+++ ascii-art-output/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"ascii-art/utils"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	// checks if there input provived
+	if len(os.Args) < 2 {
+		fmt.Println("Please provide text to display.")
+		return
+	}
+	// reads contents of the provided
+	content, err := os.ReadFile("standard.txt")
+	if err != nil {
+		fmt.Println("invalid text file")
+		return
+	}
+	contentLines := utils.SplitFile(string(content))
+	// checks if the file provided contains 856 lines
+	if len(contentLines) != 856 {
+		fmt.Println("invalid text file")
+		return
+	}
+	utils.DisplayText(strings.Join(os.Args[1:], " "), contentLines)
+}
