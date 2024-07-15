2812d1521d40acabae83f2def5061a48d735cf27
moonyango <mosesadrian825@gmail.com>
2024-07-15 16:31:38 +0300

    feat: add ascii-art project for modification

--- ascii-art-web/main.go
+++ ascii-art-web/main.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
+	"web/utils"
+)
 
+func main() {
+	// checks if there input provived
+	if len(os.Args) < 2 {
+		fmt.Println("Please provide text to display.")
+		return
+	}
+	// reads contents of the provided
+	content, err := os.ReadFile(utils.GetFile(os.Args[2]))
+	if err != nil {
+		fmt.Println("invalid text file")
+		return
+	}
+	contentLines := utils.SplitFile(string(content))
+	// checks if the file provided contains 856 lines
+	if len(contentLines) != 856 {
+		fmt.Println("invalid text file")
+		return
+	}
+	utils.DisplayText(os.Args[1], contentLines)
 }
---
+++ ascii-art-web/utils/display.go
@@ -0,0 +1,32 @@
+package utils
+
+import (
+	"fmt"
+	"strings"
+)
+
+func DisplayText(input string, contentLines []string) {
+	if input == "" {
+		return
+	}
+	if input == "\\n" || input == "\n" {
+		fmt.Println()
+		return
+	}
+	input = strings.ReplaceAll(input, "\n", "\\n")
+	input = strings.ReplaceAll(input, "\\t", "    ")
+	// split the input string with the "\\n" into a slice strings
+	wordslice := strings.Split(input, "\\n")
+
+	for _, word := range wordslice {
+		if word == "" {
+			fmt.Println()
+		} else {
+			if IsEnglish(word) {
+				PrintWord(word, contentLines)
+			} else {
+				fmt.Println("Invalid input:", word)
+			}
+		}
+	}
+}
