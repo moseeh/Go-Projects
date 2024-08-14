c559640e16996075398813981459cf19a121368a
moonyango <mosesadrian825@gmail.com>
2024-08-14 12:55:02 +0300

    fix: print to see output

--- groupie-tracker/main.go
+++ groupie-tracker/main.go
@@ -19,7 +19,22 @@ func main() {
 		fmt.Println(err)
 		return
 	}
+	Relations, err2 := utils.GetRelations(apiIndex.Relation)
+	fmt.Println(apiIndex.Relation)
+	if err2 != nil {
+		fmt.Println(err)
+		return
+	}
 	fmt.Println(Artists[0].CreationDate)
 	fmt.Println(Locations[0])
 	fmt.Println(Dates[0])
+	// Print out relations data
+	for _, relation := range Relations {
+		fmt.Printf("ID: %d\n", relation.ID)
+		for location, dates := range relation.DatesLocations {
+			fmt.Printf("  Location: %s\n", location)
+			fmt.Printf("  Dates: %v\n", dates)
+		}
+		fmt.Println("------------------------")
+	}
 }
