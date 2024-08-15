be9676859ce91f3d4bce236067443b04c2118f9a
moonyango <mosesadrian825@gmail.com>
2024-08-15 05:47:36 +0300

    change implementation from []date to date

--- groupie-tracker/utils/getDates.go
+++ groupie-tracker/utils/getDates.go
@@ -7,27 +7,27 @@ import (
 	"net/http"
 )
 
-func GetDates(url string) ([]Date, error) {
+func GetDates(url string) (Date, error) {
 	response, err := http.Get(url)
 	if err != nil {
-		return nil, fmt.Errorf("error making HTTP request: %w", err)
+		return Date{}, fmt.Errorf("error making HTTP request: %w", err)
 	}
 	defer response.Body.Close()
 
 	if response.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf("unexpected status code: %d", response.StatusCode)
+		return Date{}, fmt.Errorf("unexpected status code: %d", response.StatusCode)
 	}
 
 	bodyBytes, err := io.ReadAll(response.Body)
 	if err != nil {
-		return nil, fmt.Errorf("error reading response body: %w", err)
+		return Date{}, fmt.Errorf("error reading response body: %w", err)
 	}
 
-	var dateIndex DateIndex
-	err = json.Unmarshal(bodyBytes, &dateIndex)
+	var dates Date
+	err = json.Unmarshal(bodyBytes, &dates)
 	if err != nil {
-		return nil, fmt.Errorf("error unmarshaling JSON: %w", err)
+		return Date{}, fmt.Errorf("error unmarshaling JSON: %w", err)
 	}
 
-	return dateIndex.Index, nil
+	return dates, nil
 }
