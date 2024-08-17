21190046f9502188f3063067229fa13f74a26cac
moonyango <mosesadrian825@gmail.com>
2024-08-17 06:02:01 +0300

    fix: change implemetation from location struct to relation structs

--- groupie-tracker/handlers/locationhandler.go
+++ groupie-tracker/handlers/locationhandler.go
@@ -2,11 +2,11 @@ package handlers
 
 import (
 	"fmt"
+	"groupie-tracker/utils"
 	"net/http"
 	"strconv"
+	"strings"
 	"text/template"
-
-	"groupie-tracker/utils"
 )
 
 func LocationHandler(w http.ResponseWriter, r *http.Request) {
@@ -15,33 +15,38 @@ func LocationHandler(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		fmt.Println(err)
 	}
-	dates, err1 := utils.GetDates(locations.Dates)
+	relations, err := utils.GetRelations(utils.GetApiIndex().Relation + "/" + strconv.Itoa((id)))
 	if err != nil {
+		fmt.Println(err)
+	}
+	dates, err1 := utils.GetDates(locations.Dates)
+	if err1 != nil {
 		fmt.Println(err1)
 	}
-
 	Artists := utils.GetArtists(utils.GetApiIndex().Artists)
-
 	data := struct {
 		Artist    utils.Artist
 		Locations utils.Location
 		Dates     utils.Date
+		Relations utils.Relations
 	}{
 		Artist:    Artists[id-1],
 		Locations: locations,
 		Dates:     dates,
+		Relations: relations,
 	}
 
 	tmpl, err := template.New("locations.html").Funcs(template.FuncMap{
-		"add": func(a, b int) int {
-			return a + b
-		},
+		"hasPrefix":  strings.HasPrefix,
+		"trimPrefix": strings.TrimPrefix,
 	}).ParseFiles("templates/locations.html")
+
 	if err != nil {
 		fmt.Printf("error: %v", err)
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
+
 	err = tmpl.Execute(w, data)
 	if err != nil {
 		fmt.Printf("Template execution error: %v", err)
