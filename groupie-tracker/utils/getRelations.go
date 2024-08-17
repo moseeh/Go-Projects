c7c9ecda4c3585b46c8eead6c58ee1e16e8192df
moonyango <mosesadrian825@gmail.com>
2024-08-17 06:01:03 +0300

    fix: change implemetation from slice of relation to relations

--- groupie-tracker/utils/getRelations.go
+++ groupie-tracker/utils/getRelations.go
@@ -7,28 +7,27 @@ import (
 	"net/http"
 )
 
-func GetRelations(url string) ([]Relations, error) {
+func GetRelations(url string) (Relations, error) {
 	response, err := http.Get(url)
 	if err != nil {
-		return nil, fmt.Errorf("error making HTTP request: %w", err)
+		return Relations{}, fmt.Errorf("error making HTTP request: %w", err)
 	}
 	defer response.Body.Close()
 
 	if response.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf("unexpected status code: %d", response.StatusCode)
+		return Relations{}, fmt.Errorf("unexpected status code: %d", response.StatusCode)
 	}
 
 	bodyBytes, err := io.ReadAll(response.Body)
 	if err != nil {
-		return nil, fmt.Errorf("error reading response body: %w", err)
+		return Relations{}, fmt.Errorf("error reading response body: %w", err)
 	}
 
-
-	var RelationIndex RelationIndex
-	err = json.Unmarshal(bodyBytes, &RelationIndex)
+	var Relation Relations
+	err = json.Unmarshal(bodyBytes, &Relation)
 	if err != nil {
-		return nil, fmt.Errorf("error unmarshaling JSON: %w", err)
+		return Relations{}, fmt.Errorf("error unmarshaling JSON: %w", err)
 	}
 
-	return RelationIndex.Index, nil
+	return Relation, nil
 }
