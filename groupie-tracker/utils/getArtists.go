08c8ee748d63a6f1374083cceda133c402d70984
moonyango <mosesadrian825@gmail.com>
2024-08-18 17:59:17 +0300

    handle error

--- groupie-tracker/utils/getArtists.go
+++ groupie-tracker/utils/getArtists.go
@@ -7,31 +7,31 @@ import (
 	"net/http"
 )
 
-func GetArtists(url string) []Artist {
+func GetArtists(url string) ([]Artist, error) {
+	// Make HTTP request
 	response, err := http.Get(url)
 	if err != nil {
-		fmt.Println("Error making HTTP request:", err)
-		return nil
+		return nil, fmt.Errorf("error making HTTP request: %w", err)
 	}
 	defer response.Body.Close()
 
+	// Check status code
 	if response.StatusCode != http.StatusOK {
-		fmt.Printf("Unexpected status code: %d\n", response.StatusCode)
-		return nil
+		return nil, fmt.Errorf("unexpected status code: %d", response.StatusCode)
 	}
 
+	// Read response body
 	bodyBytes, err := io.ReadAll(response.Body)
 	if err != nil {
-		fmt.Println("Error reading response body:", err)
-		return nil
+		return nil, fmt.Errorf("error reading response body: %w", err)
 	}
 
+	// Unmarshal JSON
 	var artists []Artist
 	err = json.Unmarshal(bodyBytes, &artists)
 	if err != nil {
-		fmt.Println("Error unmarshaling JSON:", err)
-		return nil
+		return nil, fmt.Errorf("error unmarshaling JSON: %w", err)
 	}
 
-	return artists
+	return artists, nil
 }
