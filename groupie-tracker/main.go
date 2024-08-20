470153a8240c5fd04c40bb565a9ab80710dcaf23
moonyango <mosesadrian825@gmail.com>
2024-08-20 17:40:03 +0300

    feat: server handler for search

--- groupie-tracker/main.go
+++ groupie-tracker/main.go
@@ -10,7 +10,8 @@ import (
 func main() {
 	http.HandleFunc("/", handlers.ArtistHandler)
 	http.HandleFunc("/locations/", handlers.LocationHandler)
+	http.HandleFunc("/search", handlers.SearchHandler)
 	log.Println("The server is running on http://127.0.0.1:8080")
 	http.ListenAndServe(":8080", nil)
-	
+
 }
