357004589d6a4cbaa8589b902faf1117786db8c6
moonyango <mosesadrian825@gmail.com>
2024-09-18 17:52:09 +0300

    add color to directories

--- my-ls-1/utils/printShort.go
+++ my-ls-1/utils/printShort.go
@@ -8,7 +8,11 @@ import (
 
 func printShortFormat(entries []models.FileInfo, indent string) {
 	for _, entry := range entries {
-		fmt.Printf("%s%s  ", indent, entry.Name)
+		if entry.IsDir {
+			fmt.Printf("%s\033[38;2;0;120;255m%s\033[0m  ", indent, entry.Name)
+		} else {
+			fmt.Printf("%s%s  ", indent, entry.Name)
+		}
 	}
 	fmt.Println()
 }
