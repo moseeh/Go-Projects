d39ea38abbfe1438064ee2ed1b75ef7bc66d057b
moonyango <mosesadrian825@gmail.com>
2024-09-19 20:36:30 +0300

    fix: format for linkcount in long format

--- my-ls-1/utils/printLong.go
+++ my-ls-1/utils/printLong.go
@@ -9,29 +9,30 @@ import (
 
 func printLongFormat(entries []models.FileInfo, indent string) {
 	var totalBlocks int64
+	sizeLen, linkLen := 0, 0
 	for _, entry := range entries {
 		totalBlocks += (entry.Size + 511) / 512
-	}
-	fmt.Printf("%stotal %d\n", indent, totalBlocks)
-	l := 0
-
-	for _, entry := range entries {
 		size := strconv.FormatInt(entry.Size, 10)
-		if l < len(size) {
-			l = len(size)
+		if sizeLen < len(size) {
+			sizeLen = len(size)
+		}
+		linkCount := strconv.Itoa(entry.Links)
+		if linkLen < len(linkCount) {
+			linkLen = len(linkCount)
 		}
 	}
+	fmt.Printf("%stotal %d\n", indent, totalBlocks)
 	for _, entry := range entries {
 		modeStr := entry.Mode.String()
 		linkCount := strconv.Itoa(entry.Links)
 		size := strconv.FormatInt(entry.Size, 10)
 		timeStr := entry.ModTime.Format("Jan _2 15:04")
 		if entry.IsDir {
-			fmt.Printf("%s%s %s %-8s %s %*s %s \033[38;2;0;120;255m%s\033[0m\n",
-				indent, modeStr, linkCount, entry.User, entry.Group, l, size, timeStr, entry.Name)
+			fmt.Printf("%s%s %*s %-8s %s %*s %s \033[38;2;0;120;255m%s\033[0m\n",
+				indent, modeStr, linkLen, linkCount, entry.User, entry.Group, sizeLen, size, timeStr, entry.Name)
 		} else {
-			fmt.Printf("%s%s %s %-8s %s %*s %s %s\n",
-				indent, modeStr, linkCount, entry.User, entry.Group, l, size, timeStr, entry.Name)
+			fmt.Printf("%s%s %*s %-8s %s %*s %s %s\n",
+				indent, modeStr, linkLen, linkCount, entry.User, entry.Group, sizeLen, size, timeStr, entry.Name)
 		}
 
 	}
