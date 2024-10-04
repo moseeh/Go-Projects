b6e89885c2f81917767f38e14be1e107df3e9156
moonyango <mosesadrian825@gmail.com>
2024-10-04 16:07:28 +0300

    fix handlers

--- groupie-tracker/handlers/artistshandler.go
+++ groupie-tracker/handlers/artistshandler.go
@@ -1,36 +1,33 @@
 package handlers
 
 import (
-    "groupie-tracker/utils"
-    "net/http"
-    "text/template"
+	"net/http"
+	"text/template"
+
+	"groupie-tracker/utils"
 )
 
 func ArtistHandler(w http.ResponseWriter, r *http.Request) {
-    // Fetch artists
-    artists, err := utils.GetArtists(utils.GetApiIndex().Artists)
-    if err != nil {
-        http.Error(w, "Error fetching artists", http.StatusInternalServerError)
-        return
-    }
+	artists, err := utils.GetArtists(utils.GetApiIndex().Artists)
+	if err != nil {
+		http.Error(w, "Error fetching artists", http.StatusInternalServerError)
+		return
+	}
 
-    // Check if artists slice is empty
-    if len(artists) == 0 {
-        http.Error(w, "No artists found", http.StatusNotFound)
-        return
-    }
+	if len(artists) == 0 {
+		http.Error(w, "No artists found", http.StatusNotFound)
+		return
+	}
 
-    // Parse template
-    tmpl, err := template.ParseFiles("templates/index.html")
-    if err != nil {
-        http.Error(w, "Error parsing template", http.StatusInternalServerError)
-        return
-    }
+	tmpl, err := template.ParseFiles("templates/index.html")
+	if err != nil {
+		http.Error(w, "Error parsing template", http.StatusInternalServerError)
+		return
+	}
 
-    // Execute template
-    err = tmpl.Execute(w, artists)
-    if err != nil {
-        http.Error(w, "Error executing template", http.StatusInternalServerError)
-        return
-    }
+	err = tmpl.Execute(w, artists)
+	if err != nil {
+		http.Error(w, "Error executing template", http.StatusInternalServerError)
+		return
+	}
 }
