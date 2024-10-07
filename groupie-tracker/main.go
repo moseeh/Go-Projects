3d112fee06be58f42bc6c24dea64fae7e566f79d
moonyango <mosesadrian825@gmail.com>
2024-10-07 15:16:10 +0300

    fix: serve static files

--- groupie-tracker/main.go
+++ groupie-tracker/main.go
@@ -8,10 +8,11 @@ import (
 )
 
 func main() {
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
 	http.HandleFunc("/", handlers.ArtistHandler)
 	http.HandleFunc("/locations/", handlers.LocationHandler)
 	http.HandleFunc("/search", handlers.SearchHandler)
 	log.Println("The server is running on http://127.0.0.1:8080")
 	http.ListenAndServe(":8080", nil)
-
 }
