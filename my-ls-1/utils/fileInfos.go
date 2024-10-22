dffc83df6e4016809fb227eab86b30fdb5c821d1
moonyango <mosesadrian825@gmail.com>
2024-10-22 18:04:17 +0300

    fix: remove usage of filepath since it wasnt an allowed package

--- my-ls-1/utils/fileInfos.go
+++ my-ls-1/utils/fileInfos.go
@@ -3,18 +3,19 @@ package utils
 import (
 	"io/fs"
 	"os"
-	"path/filepath"
 	"strings"
 	"syscall"
 
 	"my-ls-1/models"
 )
 
+// getFileInfos retrieves file information for each directory entry in the given path.
+// It optionally includes special entries like "." and ".." if the `All` option is set.
 func getFileInfos(path string, entries []fs.DirEntry, options models.Options) []models.FileInfo {
 	fileInfos := make([]models.FileInfo, 0, len(entries))
 
 	addSpecialEntry := func(name string) {
-		info, err := os.Stat(filepath.Join(path, name))
+		info, err := os.Stat(path + "/" + name)
 		if err == nil {
 			fileInfos = append(fileInfos, getFileInfo(name, info, options))
 		}
@@ -37,10 +38,11 @@ func getFileInfos(path string, entries []fs.DirEntry, options models.Options) []
 
 		fileInfos = append(fileInfos, getFileInfo(entry.Name(), info, options))
 	}
-
 	return fileInfos
 }
 
+// getFileInfo constructs a FileInfo object based on the given file name and os.FileInfo.
+// If the `Long` option is enabled, it extracts additional metadata such as link count, user, and group.
 func getFileInfo(name string, info os.FileInfo, options models.Options) models.FileInfo {
 	fileInfo := models.FileInfo{
 		Name:    name,
@@ -53,6 +55,7 @@ func getFileInfo(name string, info os.FileInfo, options models.Options) models.F
 	if options.Long {
 		stat := info.Sys().(*syscall.Stat_t)
 		fileInfo.Links = int(stat.Nlink)
+		fileInfo.BlockSize = stat.Blocks
 		fileInfo.User = getUserName(int(stat.Uid))
 		fileInfo.Group = getGroupName(int(stat.Gid))
 	}
