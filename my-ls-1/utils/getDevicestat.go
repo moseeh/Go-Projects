72f3168e4547ec9de13576c1a3e926a93b96221c
moonyango <mosesadrian825@gmail.com>
2024-10-22 18:05:16 +0300

    chore: add comments

--- my-ls-1/utils/getDevicestat.go
+++ my-ls-1/utils/getDevicestat.go
@@ -5,6 +5,8 @@ import (
 	"syscall"
 )
 
+// getDeviceStat retrieves the device statistics for the specified file path.
+// It returns a pointer to a `syscall.Stat_t` structure containing the file's metadata.
 func getDeviceStat(filePath string) *syscall.Stat_t {
 	stat := &syscall.Stat_t{}
 	err := syscall.Lstat(filePath, stat)
@@ -14,6 +16,8 @@ func getDeviceStat(filePath string) *syscall.Stat_t {
 	return stat
 }
 
+// majorMinor extracts the major and minor device numbers from the raw device ID (rdev).
+// It returns the major and minor numbers separately for easier processing and display.
 func majorMinor(rdev uint64) (uint64, uint64) {
 	major := (rdev >> 8) & 0xfff
 	minor := (rdev & 0xff) | ((rdev >> 12) & 0xfff00)
