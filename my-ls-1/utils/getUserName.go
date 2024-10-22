95f9bb75ebfe7cedbb1479434caee089a9a58dbe
moonyango <mosesadrian825@gmail.com>
2024-10-22 18:09:17 +0300

    chore add commit for getusername function

--- my-ls-1/utils/getUserName.go
+++ my-ls-1/utils/getUserName.go
@@ -5,6 +5,8 @@ import (
 	"strconv"
 )
 
+// getUserName retrieves the username associated with the given user ID (uid).
+// If the user ID cannot be found, it returns the user ID as a string.
 func getUserName(uid int) string {
 	u, err := user.LookupId(strconv.Itoa(uid))
 	if err != nil {
