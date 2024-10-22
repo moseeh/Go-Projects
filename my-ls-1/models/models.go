e9b6923e49cc72a83321339aafbfb1edfe81ae29
moonyango <mosesadrian825@gmail.com>
2024-10-22 18:02:56 +0300

    fix: format and and block size to struct

--- my-ls-1/models/models.go
+++ my-ls-1/models/models.go
@@ -5,17 +5,20 @@ import (
 	"time"
 )
 
+// infomation about the flag
 type FileInfo struct {
-	Name    string
-	Mode    os.FileMode
-	Size    int64
-	ModTime time.Time
-	IsDir   bool
-	User    string
-	Group   string
-	Links   int
+	Name      string
+	Mode      os.FileMode
+	Size      int64
+	ModTime   time.Time
+	IsDir     bool
+	User      string
+	Group     string
+	Links     int
+	BlockSize int64
 }
 
+// flags to use
 type Options struct {
 	Long       bool
 	Recursive  bool
