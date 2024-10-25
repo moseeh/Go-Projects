2080e338982a7d346259409dcf12030dc96beb20
moonyango <mosesadrian825@gmail.com>
2024-10-25 18:56:22 +0300

    feat: function to convert string values to float values

---
+++ linear-stats/utils/convtoFloat.go
@@ -0,0 +1,23 @@
+package utils
+
+import (
+	"fmt"
+	"strconv"
+)
+
+// converting a slice of string to a slice of int
+func ConvToFloat(sArr []string) []float64 {
+	if len(sArr) == 0 {
+		return nil
+	}
+	nArr := []float64{}
+	for _, v := range sArr {
+		num, err := strconv.ParseFloat(v, 32)
+		if err != nil {
+			fmt.Println("error converting number", v)
+		} else {
+			nArr = append(nArr, num)
+		}
+	}
+	return nArr
+}
