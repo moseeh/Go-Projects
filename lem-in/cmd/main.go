3a9fe8be051de1eea6a93c1b8a9e4402227a65b2
moonyango <mosesadrian825@gmail.com>
2024-10-31 16:38:52 +0300

    incorporate all these functions in the main function

--- lem-in/cmd/main.go
+++ lem-in/cmd/main.go
@@ -1,4 +1,22 @@
 package main
 
+import (
+	"fmt"
+
+	"lem-in/utils"
+)
+
 func main() {
+	filename, errmsg := utils.ParseArgs()
+	if errmsg != "" {
+		fmt.Println(errmsg)
+		return
+	}
+	Antcolony, err := utils.ParseFile(filename)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	paths := utils.FindPaths(Antcolony)
+	fmt.Println(paths)
 }
