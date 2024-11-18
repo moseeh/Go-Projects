413f8848b14b764a271f288536604000e0dccdf3
moonyango <mosesadrian825@gmail.com>
2024-11-18 18:33:41 +0300

    take command line args that specify port number

--- net-cat/cmd/main.go
+++ net-cat/cmd/main.go
@@ -1,4 +1,23 @@
 package main
 
+import (
+	"fmt"
+	"os"
+
+	"net-cat/server"
+)
+
 func main() {
+	args := os.Args[1:]
+	var port string
+	if len(args) > 1 {
+		fmt.Println("[USAGE]: ./TCPChat $port")
+		return
+	}
+	if len(args) == 1 {
+		port = ":" + args[0]
+	} else {
+		port = ":8989"
+	}
+	server.Server(port)
 }
