79f52c87e1baee0dbb83e0540bddea7376530bfc
moonyango <mosesadrian825@gmail.com>
2024-11-18 18:31:23 +0300

    feat: function to broadcast client message to all other clients

---
+++ net-cat/client/broadcastclient.go
@@ -0,0 +1,25 @@
+package client
+
+import (
+	"fmt"
+
+	"net-cat/models"
+)
+
+func BroadcastJoin(name string) {
+	// Notify all other models.Clients that a new client has joined
+	for n, c := range models.Clients {
+		if n != name {
+			fmt.Fprintf(c, "[%s] has joined the chat\n", name)
+		}
+	}
+}
+
+func BroadcastLeave(name string) {
+	// Notify all other models.Clients that a client has left
+	for n, c := range models.Clients {
+		if n != name {
+			fmt.Fprintf(c, "[%s] has left the chat\n", name)
+		}
+	}
+}
