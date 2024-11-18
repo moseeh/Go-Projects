d10363d0b695939a0b7a8a695de945cb2acef5b0
moonyango <mosesadrian825@gmail.com>
2024-11-18 18:33:09 +0300

    feat: function that sets up a listener for connections while handling them

---
+++ net-cat/server/server.go
@@ -0,0 +1,91 @@
+package server
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"net"
+	"strings"
+	"time"
+
+	"net-cat/client"
+	"net-cat/models"
+	"net-cat/utils"
+)
+
+type newServer struct {
+	models.Server
+}
+
+func Server(addr string) {
+	server := newServer{
+		Server: models.Server{
+			Prevchat: make([]string, 0),
+		},
+	}
+
+	listener, err := net.Listen("tcp", addr)
+	if err != nil {
+		fmt.Println("error listening", err)
+	}
+	fmt.Printf("Listening on port %s  ...\n", addr)
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("error accepting connections")
+			continue
+		}
+		fmt.Println(len(models.Clients))
+		go server.handleConnection(conn)
+	}
+}
+
+func (s *newServer) handleConnection(conn net.Conn) {
+	defer conn.Close()
+	fmt.Println("New connection from", conn.RemoteAddr())
+	conn.Write(utils.ReadLogo())
+	name, err := client.GetClientName(conn)
+	if err != nil {
+		fmt.Println("Error getting client name:", err)
+		return
+	}
+
+	// Add the client to the chat
+	client.AddClient(name, conn)
+	defer client.RemoveClient(name)
+
+	// Handle messages from the client
+	s.handleMessages(name, conn)
+}
+
+func (s *newServer) handleMessages(name string, conn net.Conn) {
+	reader := bufio.NewReader(conn)
+	for {
+		msg, err := reader.ReadString('\n')
+		if err != nil {
+			if err != io.EOF {
+				fmt.Println("Error reading message:", err)
+			}
+			return
+		}
+
+		if strings.TrimSpace(msg) == "" {
+			continue
+		}
+
+		broadcastMessage(name, msg)
+	}
+}
+
+func broadcastMessage(name, msg string) {
+	timestamp := time.Now().Format("2006-01-02 15:04:05")
+	formattedMsg := fmt.Sprintf("[%s][%s]: %s", timestamp, name, msg)
+
+	models.ClientsMutex.Lock()
+	defer models.ClientsMutex.Unlock()
+	for n, c := range models.Clients {
+		if n != name {
+			fmt.Fprint(c, formattedMsg)
+		}
+	}
+}
