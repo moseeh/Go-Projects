c2838b71bc345db2c9b8aa8fad58cd673aebf03c
moonyango <mosesadrian825@gmail.com>
2024-11-19 18:07:52 +0300

    fix: change implemetation to confirm name hasnt been used

--- net-cat/client/addclient.go
+++ net-cat/client/addclient.go
@@ -1,21 +1,34 @@
 package client
 
 import (
+	"errors"
 	"net"
 
 	"net-cat/models"
 )
 
-func AddClient(name string, conn net.Conn) {
+// AddClient attempts to add a new client to the Clients map.
+// It returns an error if the name is already in use.
+func AddClient(name string, conn net.Conn) error {
 	models.ClientsMutex.Lock()
 	defer models.ClientsMutex.Unlock()
+
+	if _, exists := models.Clients[name]; exists {
+		return errors.New("name already in use")
+	}
+
 	models.Clients[name] = conn
 	BroadcastJoin(name)
+	return nil
 }
 
+// RemoveClient removes a client from the Clients map.
 func RemoveClient(name string) {
 	models.ClientsMutex.Lock()
 	defer models.ClientsMutex.Unlock()
-	delete(models.Clients, name)
-	BroadcastLeave(name)
+
+	if _, exists := models.Clients[name]; exists {
+		delete(models.Clients, name)
+		BroadcastLeave(name)
+	}
 }
