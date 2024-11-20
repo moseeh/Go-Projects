4872e61a8d2dd599acd523427f4c7569ef308e17
moonyango <mosesadrian825@gmail.com>
2024-11-20 17:52:15 +0300

    feat: new function to find turns

---
+++ lem-in/utils/printTurns.go
@@ -0,0 +1,14 @@
+package utils
+
+// import (
+// 	"lem-in/models"
+// )
+
+// func PrintTurns(paths []models.Path, distributions []int, numOfAnts int) {
+// 	antno := 0
+// 	for numOfAnts > 0 {
+// 		for i, path := range paths {
+			
+// 		}
+// 	}
+// }
