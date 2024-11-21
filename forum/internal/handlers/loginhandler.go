32435d9a7c7f60f42c34bd6772f80f9efee9c337
moonyango <mosesadrian825@gmail.com>
2024-11-21 16:10:14 +0300

    feat: handler for login page

---
+++ forum/internal/handlers/loginhandler.go
@@ -0,0 +1,18 @@
+package handlers
+
+import (
+	"html/template"
+	"net/http"
+)
+
+func LoginHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl, err := template.ParseFiles("templates/login.html")
+	if err != nil {
+		http.Error(w, "Error parsing template: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+	err = tmpl.Execute(w, nil)
+	if err != nil {
+		http.Error(w, "Error executing template: "+err.Error(), http.StatusInternalServerError)
+	}
+}
