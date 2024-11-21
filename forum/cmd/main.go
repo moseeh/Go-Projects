3eee1e86b15303e3e33956f44a18c02be2857aba
moonyango <mosesadrian825@gmail.com>
2024-11-21 15:57:40 +0300

    feat: call handlers

--- forum/cmd/main.go
+++ forum/cmd/main.go
@@ -1,4 +1,16 @@
 package main
 
+import (
+	"log"
+	"net/http"
+
+	"forum/internal/handlers"
+)
+
 func main() {
+	http.HandleFunc("/", handlers.HomeHandler)
+	http.HandleFunc("/login", handlers.LoginHandler)
+	http.HandleFunc("/signup", handlers.SignupHandler)
+	log.Println("The server is running on http://127.0.0.1:8080")
+	http.ListenAndServe(":8080", nil)
 }
