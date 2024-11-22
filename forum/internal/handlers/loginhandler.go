1e20021ac64890083bd6b6ce6fd22c42fc9c0209
moonyango <mosesadrian825@gmail.com>
2024-11-22 18:12:31 +0300

    fix: loginhandler to verify user login details

--- forum/internal/handlers/loginhandler.go
+++ forum/internal/handlers/loginhandler.go
@@ -3,16 +3,62 @@ package handlers
 import (
 	"html/template"
 	"net/http"
+
+	"forum/internal/database"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
-func LoginHandler(w http.ResponseWriter, r *http.Request) {
-	tmpl, err := template.ParseFiles("templates/login.html")
-	if err != nil {
-		http.Error(w, "Error parsing template: "+err.Error(), http.StatusInternalServerError)
-		return
+type Login struct {
+	UserRepo *database.UserRepository
+}
+
+type LoginCredentials struct {
+	Username string
+	Password string
+}
+
+func NewLoginHandler(userRepo *database.UserRepository) *Login {
+	return &Login{
+		UserRepo: userRepo,
 	}
-	err = tmpl.Execute(w, nil)
-	if err != nil {
-		http.Error(w, "Error executing template: "+err.Error(), http.StatusInternalServerError)
+}
+
+func (h *Login) LoginHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		tmpl, err := template.ParseFiles("templates/login.html")
+		if err != nil {
+			http.Error(w, "Error parsing template: "+err.Error(), http.StatusInternalServerError)
+			return
+		}
+		err = tmpl.Execute(w, nil)
+		if err != nil {
+			http.Error(w, "Error executing template: "+err.Error(), http.StatusInternalServerError)
+		}
+	}
+	if r.Method == http.MethodPost {
+		username := r.FormValue("username")
+		password := r.FormValue("password")
+		if username == "" || password == "" {
+			http.Error(w, "Username and password are required", http.StatusBadRequest)
+			return
+		}
+
+		user, err := h.UserRepo.GetUserByUsername(username)
+		if err != nil {
+			http.Error(w, "Error Finding User", http.StatusInternalServerError)
+			return
+		}
+		if user == nil {
+			http.Error(w, "Invalid Username or password", http.StatusUnauthorized)
+			return
+		}
+		err = bcrypt.CompareHashAndPassword([]byte(user.PasswordHash), []byte(password))
+		if err != nil {
+			// Password doesn't match
+			http.Error(w, "Invalid username or password", http.StatusUnauthorized)
+			return
+		}
+		http.Redirect(w, r, "/", http.StatusSeeOther)
 	}
 }
